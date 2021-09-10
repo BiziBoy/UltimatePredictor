811d990eaafc5307432dc8ba091bd42b30713a8a
BiziBoy <79360682+BiziBoy@users.noreply.github.com>
2021-09-10 10:14:13 +0300

    Добавил проценты загрузки, подключил библиотеку для считывания предсказаний из json файла

--- UltimatePredictor/Form1.cs
+++ UltimatePredictor/Form1.cs
@@ -8,11 +8,16 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace UltimatePredictor
 {
   public partial class Form1 : Form
   {
+    private const string APP_NAME = "ULTIMATE_PREDICTOR";
+    private readonly string PREDICTIONS_CONFIG_PATH = $"{Environment.CurrentDirectory}\\predictionsConfig.json";
+    private string[] _predictions;
     public Form1()
     {
       InitializeComponent();
@@ -20,6 +25,7 @@ namespace UltimatePredictor
 
     private async void bPredict1_Click(object sender, EventArgs e)
     {
+      bPredict1.Enabled = false;
       await Task.Run(() =>
       {
         for (int i = 0; i < 100; i++)
@@ -27,15 +33,21 @@ namespace UltimatePredictor
           Invoke(new Action(() =>
           {
             updateProgressBar(i);
+            this.Text = $"{i + 1}%";
+
           }));
 
-          Thread.Sleep(100);
+          Thread.Sleep(50);
         }
       });
 
-      MessageBox.Show("prediction");
+      MessageBox.Show("Ты пидор.");
+      progressBar1.Value = 0;
+      this.Text = APP_NAME;
+      bPredict1.Enabled = true;
+
     }
-    
+
     private void updateProgressBar(int i)
     {
       if (i == progressBar1.Maximum)
@@ -55,6 +67,26 @@ namespace UltimatePredictor
 
     private void progressBar1_Click(object sender, EventArgs e)
     {
+
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      this.Text = APP_NAME;
+      try
+      {
+        var data = File.ReadAllText(PREDICTIONS_CONFIG_PATH, Encoding.GetEncoding("windows-1251"));
+        _predictions = JsonConvert.DeserializeObject<string[]>(data); 
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.Message);
+        throw;
+      }
+      finally
+      {
+
+      }
 
     }
   }
